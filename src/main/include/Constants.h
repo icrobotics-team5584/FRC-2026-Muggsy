686ae1ee3cac6347430b7758bc4eae5178b7c1d4
furmea <diaschithira21@gmail.com>
2026-04-10 22:03:43 +1000

    Reapply "Reapply "skeleton climber with dummy placerholder values""
    This reverts commit 5687bf19b50c4256f3ad9e2bb388944253bd6a11.

--- src/main/include/Constants.h
+++ src/main/include/Constants.h
@@ -17,7 +17,10 @@ namespace canid {
     constexpr int DRIVEBASE_BACK_LEFT_TURN = 11;
     constexpr int DRIVEBASE_BACK_LEFT_ENCODER = 12; 
 
-    constexpr int PIGEON_2 = 13; 
+    constexpr int PIGEON_2 = 13;
+    
+    constexpr int CLIMBER_MOTOR_1 = 14;
+    constexpr int CLIMBER_MOTOR_2 = 15; /* Follower */
 
     constexpr int PDH = 40;
 }
