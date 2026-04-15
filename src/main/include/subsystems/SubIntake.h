13392efb0ad8d9ac413c9e1350a596a52be7a6f7
furmea <diaschithira21@gmail.com>
2026-04-15 17:02:07 +1000

    got neos mixed up with falcons, my bad-- fixed now

--- src/main/include/subsystems/SubIntake.h
+++ src/main/include/subsystems/SubIntake.h
@@ -2,10 +2,10 @@
 
 #include <frc2/command/SubsystemBase.h>
 #include <frc2/command/Commands.h>
-#include <rev/config/SparkFlexConfigAccessor.h>
-#include <rev/config/SparkFlexConfig.h>
+#include <ctre/phoenix6/TalonFX.hpp>
+#include <ctre/phoenix6/signals/SpnEnums.hpp>
+#include <units/current.h>
 
-#include "utilities/ICSparkFlex.h"
 #include "Constants.h"
 
 class SubIntake : public frc2::SubsystemBase {
@@ -23,16 +23,19 @@ class SubIntake : public frc2::SubsystemBase {
 
 		bool isIntakeEnabled();
 	private:
-		ICSparkFlex _motor1{canid::INTAKE_MOTOR_1};
-		ICSparkFlex _motor2{canid::INTAKE_MOTOR_2};
-		rev::spark::SparkFlexConfig _motorConfig;
-		rev::spark::SparkBaseConfig::IdleMode _IDLE_MODE =
-			rev::spark::SparkBaseConfig::IdleMode::kBrake;
-    	
-		static constexpr units::ampere_t _CURRENT_LIMIT = 60_A;
-		static constexpr bool _INVERTED = false; /* _motor2 inverts this */
+		ctre::phoenix6::hardware::TalonFX _motor1{canid::INTAKE_MOTOR_1};
+		ctre::phoenix6::hardware::TalonFX _motor2{canid::INTAKE_MOTOR_2};
+		static constexpr ctre::phoenix6::signals::NeutralModeValue _IDLE_MODE =
+			ctre::phoenix6::signals::NeutralModeValue::Coast;
+		static constexpr ctre::phoenix6::signals::InvertedValue _INVERTED =
+			ctre::phoenix6::signals::InvertedValue::Clockwise_Positive;
+
+        static constexpr units::ampere_t _SUPPLY_CURRENT_LIMT = 40_A;
+        static constexpr units::ampere_t _SUPPLY_CURRENT_LOWER_LIMIT = 35_A;
+        static constexpr units::ampere_t _STATOR_CURRENT_LIMIT = 60_A;
+        static constexpr units::second_t _SUPPLY_CURRENT_LOWER_TIME = 0.1_s;
 		static constexpr double _P = 1.0;
 		static constexpr double _I = 0.0;
 		static constexpr double _D = 0.0;
-		static constexpr double _GEAR_RATIO = 3;
+		static constexpr double _GEAR_RATIO = 0.5; /* 2:1 */
 };
