590c9d41a8a2a590dfcf3e50680df970eefad5d8
furmea <diaschithira21@gmail.com>
2026-04-15 18:44:02 +1000

    clang-format

--- src/main/cpp/subsystems/SubIntake.cpp
+++ src/main/cpp/subsystems/SubIntake.cpp
@@ -3,47 +3,44 @@
 #include "utilities/Logger.h"
 
 SubIntake::SubIntake() {
-	ctre::phoenix6::configs::TalonFXConfiguration config;
+  ctre::phoenix6::configs::TalonFXConfiguration config;
 
-	config.Feedback.SensorToMechanismRatio = 1 / _GEAR_RATIO;
-	config.Slot0.kP = _P;
-	config.Slot0.kI = _I;
-	config.Slot0.kD = _D;
-	config.MotorOutput.Inverted = _INVERTED;
-	config.MotorOutput.NeutralMode = _IDLE_MODE;
-	config.CurrentLimits.SupplyCurrentLimitEnable = true;
-	config.CurrentLimits.SupplyCurrentLimit = _SUPPLY_CURRENT_LIMT;
-	config.CurrentLimits.SupplyCurrentLowerLimit = _SUPPLY_CURRENT_LOWER_LIMIT;
-	config.CurrentLimits.SupplyCurrentLowerTime = _SUPPLY_CURRENT_LOWER_TIME;
-	config.CurrentLimits.StatorCurrentLimitEnable = true;
-	config.CurrentLimits.StatorCurrentLimit = _STATOR_CURRENT_LIMIT;
-	
-	_motor1.GetConfigurator().Apply(config);
-	_motor2.SetControl(ctre::phoenix6::controls::Follower(
-		canid::INTAKE_MOTOR_2,
-		ctre::phoenix6::signals::MotorAlignmentValue::Opposed));
+  config.Feedback.SensorToMechanismRatio = 1 / _GEAR_RATIO;
+  config.Slot0.kP = _P;
+  config.Slot0.kI = _I;
+  config.Slot0.kD = _D;
+  config.MotorOutput.Inverted = _INVERTED;
+  config.MotorOutput.NeutralMode = _IDLE_MODE;
+  config.CurrentLimits.SupplyCurrentLimitEnable = true;
+  config.CurrentLimits.SupplyCurrentLimit = _SUPPLY_CURRENT_LIMT;
+  config.CurrentLimits.SupplyCurrentLowerLimit = _SUPPLY_CURRENT_LOWER_LIMIT;
+  config.CurrentLimits.SupplyCurrentLowerTime = _SUPPLY_CURRENT_LOWER_TIME;
+  config.CurrentLimits.StatorCurrentLimitEnable = true;
+  config.CurrentLimits.StatorCurrentLimit = _STATOR_CURRENT_LIMIT;
+
+  _motor1.GetConfigurator().Apply(config);
+  _motor2.SetControl(ctre::phoenix6::controls::Follower(
+    canid::INTAKE_MOTOR_2, ctre::phoenix6::signals::MotorAlignmentValue::Opposed));
 }
 
-
 void SubIntake::Periodic() {
-	Logger::LogFalcon("Intake/Motor1", _motor1);
-	Logger::LogFalcon("Intake/Motor2", _motor2);
-	
-	Logger::Log("Intake/Is Intaking", 
-		_motor1.GetMotorVoltage().GetValue() == 0_V &&
-		_motor2.GetMotorVoltage().GetValue() == 0_V);
+  Logger::LogFalcon("Intake/Motor1", _motor1);
+  Logger::LogFalcon("Intake/Motor2", _motor2);
+
+  Logger::Log("Intake/Is Intaking",
+    _motor1.GetMotorVoltage().GetValue() == 0_V && _motor2.GetMotorVoltage().GetValue() == 0_V);
 }
 
 frc2::CommandPtr SubIntake::IntakeEnable() {
-	return frc2::cmd::RunOnce([this] {
-		_motor1.SetControl(ctre::phoenix6::controls::VoltageOut{5_V});
-		_motor2.SetControl(ctre::phoenix6::controls::VoltageOut{5_V});
-	});
+  return frc2::cmd::RunOnce([this] {
+    _motor1.SetControl(ctre::phoenix6::controls::VoltageOut{5_V});
+    _motor2.SetControl(ctre::phoenix6::controls::VoltageOut{5_V});
+  });
 }
 
 frc2::CommandPtr SubIntake::IntakeDisable() {
-	return frc2::cmd::RunOnce([this] {
-		_motor1.SetControl(ctre::phoenix6::controls::VoltageOut{0_V});
-		_motor2.SetControl(ctre::phoenix6::controls::VoltageOut{0_V});
-	});
+  return frc2::cmd::RunOnce([this] {
+    _motor1.SetControl(ctre::phoenix6::controls::VoltageOut{0_V});
+    _motor2.SetControl(ctre::phoenix6::controls::VoltageOut{0_V});
+  });
 }
--- src/main/include/subsystems/SubIntake.h
+++ src/main/include/subsystems/SubIntake.h
@@ -1,7 +1,8 @@
 #pragma once
 
-#include <frc2/command/SubsystemBase.h>
 #include <frc2/command/Commands.h>
+#include <frc2/command/SubsystemBase.h>
+
 #include <ctre/phoenix6/TalonFX.hpp>
 #include <ctre/phoenix6/signals/SpnEnums.hpp>
 #include <units/current.h>
@@ -9,33 +10,34 @@
 #include "Constants.h"
 
 class SubIntake : public frc2::SubsystemBase {
-	public:
-    	static SubIntake& GetInstance() {
-        	static SubIntake inst;
-        	return inst;
-    	}
-		SubIntake();
+ public:
+  static SubIntake& GetInstance() {
+    static SubIntake inst;
+    return inst;
+  }
+  SubIntake();
 
-		void Periodic() override;
+  void Periodic() override;
 
-		frc2::CommandPtr IntakeEnable();
-		frc2::CommandPtr IntakeDisable();
+  frc2::CommandPtr IntakeEnable();
+  frc2::CommandPtr IntakeDisable();
 
-		bool isIntakeEnabled();
-	private:
-		ctre::phoenix6::hardware::TalonFX _motor1{canid::INTAKE_MOTOR_1};
-		ctre::phoenix6::hardware::TalonFX _motor2{canid::INTAKE_MOTOR_2};
-		static constexpr ctre::phoenix6::signals::NeutralModeValue _IDLE_MODE =
-			ctre::phoenix6::signals::NeutralModeValue::Coast;
-		static constexpr ctre::phoenix6::signals::InvertedValue _INVERTED =
-			ctre::phoenix6::signals::InvertedValue::Clockwise_Positive;
+  bool isIntakeEnabled();
 
-        static constexpr units::ampere_t _SUPPLY_CURRENT_LIMT = 40_A;
-        static constexpr units::ampere_t _SUPPLY_CURRENT_LOWER_LIMIT = 35_A;
-        static constexpr units::ampere_t _STATOR_CURRENT_LIMIT = 60_A;
-        static constexpr units::second_t _SUPPLY_CURRENT_LOWER_TIME = 0.1_s;
-		static constexpr double _P = 1.0;
-		static constexpr double _I = 0.0;
-		static constexpr double _D = 0.0;
-		static constexpr double _GEAR_RATIO = 0.5; /* 2:1 */
+ private:
+  ctre::phoenix6::hardware::TalonFX _motor1{canid::INTAKE_MOTOR_1};
+  ctre::phoenix6::hardware::TalonFX _motor2{canid::INTAKE_MOTOR_2};
+  static constexpr ctre::phoenix6::signals::NeutralModeValue _IDLE_MODE =
+    ctre::phoenix6::signals::NeutralModeValue::Coast;
+  static constexpr ctre::phoenix6::signals::InvertedValue _INVERTED =
+    ctre::phoenix6::signals::InvertedValue::Clockwise_Positive;
+
+  static constexpr units::ampere_t _SUPPLY_CURRENT_LIMT = 40_A;
+  static constexpr units::ampere_t _SUPPLY_CURRENT_LOWER_LIMIT = 35_A;
+  static constexpr units::ampere_t _STATOR_CURRENT_LIMIT = 60_A;
+  static constexpr units::second_t _SUPPLY_CURRENT_LOWER_TIME = 0.1_s;
+  static constexpr double _P = 1.0;
+  static constexpr double _I = 0.0;
+  static constexpr double _D = 0.0;
+  static constexpr double _GEAR_RATIO = 0.5; /* 2:1 */
 };
