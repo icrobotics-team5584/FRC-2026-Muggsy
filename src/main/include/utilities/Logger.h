00d59f18fcdf83c080104c61dbbff5ffd670b8fe
furmea <diaschithira21@gmail.com>
2026-04-29 20:33:05 +1000

    const &-ify && std::move && remove return-after-else logger

--- src/main/include/utilities/Logger.h
+++ src/main/include/utilities/Logger.h
@@ -22,7 +22,7 @@
 #include <frc/trajectory/Trajectory.h>
 #include <frc2/command/CommandScheduler.h>
 
-namespace Logger {
+namespace logger {
 void LogFalcon(std::string name, ctre::phoenix6::hardware::TalonFX& talonFX);
 
 void Log(std::string_view keyName, wpi::Sendable* data);
@@ -33,45 +33,45 @@ void Log(std::string_view keyName, bool value);
 void Log(std::string_view keyName, std::string_view value);
 void Log(std::string_view keyName, frc::DriverStation::Alliance value);
 void Log(std::string_view keyName, wpi::array<frc::SwerveModuleState, 4> states);
-void Log(std::string keyName, units::turn_t value);
-void Log(std::string keyName, ctre::phoenix6::StatusSignal<units::turn_t>& signal);
-void Log(std::string keyName, units::degree_t value);
-void Log(std::string keyName, units::turns_per_second_t value);
-void Log(std::string keyName, ctre::phoenix6::StatusSignal<units::turns_per_second_t>& signal);
-void Log(std::string keyName, units::turns_per_second_squared_t value);
-void Log(std::string keyName, ctre::phoenix6::StatusSignal<units::turns_per_second_squared_t>& signal);
-void Log(std::string keyName, units::meter_t value);
-void Log(std::string keyName, units::meters_per_second_t value);
-void Log(std::string keyName, units::meters_per_second_squared_t value);
-void Log(std::string keyName, units::volt_t value);
-void Log(std::string keyName, ctre::phoenix6::StatusSignal<units::volt_t>& signal);
-void Log(std::string keyName, units::ampere_t value);
-void Log(std::string keyName, ctre::phoenix6::StatusSignal<units::ampere_t>& signal);
-void Log(std::string keyName, units::second_t value);
-void Log(std::string keyName, ctre::phoenix6::StatusSignal<units::second_t>& signal);
-void Log(std::string keyName, units::kilogram_t value);
-void Log(std::string keyName, units::celsius_t value);
-void Log(std::string keyName, ctre::phoenix6::StatusSignal<units::celsius_t>& signal);
-void Log(std::string keyName, frc::Rotation2d value);
-void Log(std::string keyName, units::radian_t value);
+void Log(const std::string &keyName, units::turn_t value);
+void Log(const std::string &keyName, ctre::phoenix6::StatusSignal<units::turn_t>& signal);
+void Log(const std::string &keyName, units::degree_t value);
+void Log(const std::string &keyName, units::turns_per_second_t value);
+void Log(const std::string &keyName, ctre::phoenix6::StatusSignal<units::turns_per_second_t>& signal);
+void Log(const std::string &keyName, units::turns_per_second_squared_t value);
+void Log(const std::string &keyName, ctre::phoenix6::StatusSignal<units::turns_per_second_squared_t>& signal);
+void Log(const std::string &keyName, units::meter_t value);
+void Log(const std::string &keyName, units::meters_per_second_t value);
+void Log(const std::string &keyName, units::meters_per_second_squared_t value);
+void Log(const std::string &keyName, units::volt_t value);
+void Log(const std::string &keyName, ctre::phoenix6::StatusSignal<units::volt_t>& signal);
+void Log(const std::string &keyName, units::ampere_t value);
+void Log(const std::string &keyName, ctre::phoenix6::StatusSignal<units::ampere_t>& signal);
+void Log(const std::string &keyName, units::second_t value);
+void Log(const std::string &keyName, ctre::phoenix6::StatusSignal<units::second_t>& signal);
+void Log(const std::string &keyName, units::kilogram_t value);
+void Log(const std::string &keyName, units::celsius_t value);
+void Log(const std::string &keyName, ctre::phoenix6::StatusSignal<units::celsius_t>& signal);
+void Log(const std::string &keyName, frc::Rotation2d value);
+void Log(const std::string &keyName, units::radian_t value);
 
 
-double Tune(std::string keyName, double defaultValue);
-bool Tune(std::string keyName, bool defaultValue);
-std::string Tune(std::string keyName, std::string defaultValue);
-units::turn_t Tune(std::string keyName, units::turn_t defaultValue);
-units::degree_t Tune(std::string keyName, units::degree_t defaultValue);
-units::turns_per_second_t Tune(std::string keyName, units::turns_per_second_t defaultValue);
-units::turns_per_second_squared_t Tune(std::string keyName, units::turns_per_second_squared_t defaultValue);
-units::meter_t Tune(std::string keyName, units::meter_t defaultValue);
-units::meters_per_second_t Tune(std::string keyName, units::meters_per_second_t defaultValue);
-units::meters_per_second_squared_t Tune(std::string keyName, units::meters_per_second_squared_t defaultValue);
-units::volt_t Tune(std::string keyName, units::volt_t defaultValue);
-units::ampere_t Tune(std::string keyName, units::ampere_t defaultValue);
-units::second_t Tune(std::string keyName, units::second_t defaultValue);
-units::kilogram_t Tune(std::string keyName, units::kilogram_t defaultValue);
-units::celsius_t Tune(std::string keyName, units::celsius_t defaultValue);
-frc::Rotation2d Tune(std::string keyName, frc::Rotation2d defaultValue);
+double Tune(const std::string &keyName, double defaultValue);
+bool Tune(const std::string &keyName, bool defaultValue);
+std::string Tune(const std::string &keyName, const std::string &defaultValue);
+units::turn_t Tune(const std::string &keyName, units::turn_t defaultValue);
+units::degree_t Tune(const std::string &keyName, units::degree_t defaultValue);
+units::turns_per_second_t Tune(const std::string &keyName, units::turns_per_second_t defaultValue);
+units::turns_per_second_squared_t Tune(const std::string &keyName, units::turns_per_second_squared_t defaultValue);
+units::meter_t Tune(const std::string &keyName, units::meter_t defaultValue);
+units::meters_per_second_t Tune(const std::string &keyName, units::meters_per_second_t defaultValue);
+units::meters_per_second_squared_t Tune(const std::string &keyName, units::meters_per_second_squared_t defaultValue);
+units::volt_t Tune(const std::string &keyName, units::volt_t defaultValue);
+units::ampere_t Tune(const std::string &keyName, units::ampere_t defaultValue);
+units::second_t Tune(const std::string &keyName, units::second_t defaultValue);
+units::kilogram_t Tune(const std::string &keyName, units::kilogram_t defaultValue);
+units::celsius_t Tune(const std::string &keyName, units::celsius_t defaultValue);
+frc::Rotation2d Tune(const std::string &keyName, frc::Rotation2d defaultValue);
 
 class FieldDisplay {
     public:
@@ -83,8 +83,8 @@ class FieldDisplay {
     }
 
     void SetRobotPose(frc::Pose2d pose);
-    void DisplayPose(std::string label, frc::Pose2d pose);
-    void DisplayTrajectory(std::string name, frc::Trajectory trajectory);
+    void DisplayPose(const std::string& label, frc::Pose2d pose);
+    void DisplayTrajectory(const std::string& name, const frc::Trajectory& trajectory);
 
     private:
     frc::Field2d _fieldDisplay;
