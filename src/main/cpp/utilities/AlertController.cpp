878fd9b8f3e7e4135cbe2ee9ca91944341e03f76
Noah <noahrjenkinson@gmail.com>
2026-04-29 20:17:19 +1000

    Restructured AlertController

--- src/main/cpp/utilities/AlertController.cpp
+++ src/main/cpp/utilities/AlertController.cpp
@@ -1,29 +1,62 @@
 #include "utilities/AlertController.h"
 
+#include <unordered_map>
 namespace AlertController {
-
-// updates the alert for the temperature. automatically updates to true or false
-void UpdateTemperatureAlert(MotorAlertConfig& config, units::celsius_t motorTemperature) {
-  if (motorTemperature > config.maxDegrees) {
-    config.temperatureAlert.Set(true);
-    config.recordedTemperatureAlert.Set(true);
-  } else {
-    config.temperatureAlert.Set(false);
-  }
+std::vector<std::weak_ptr<AlertConfig>> configList;
+// Registers the Alert Config to be used in the ForceRemoveAllAlerts Command
+void RegisterAlertConfig(std::weak_ptr<AlertConfig> config) {
+  configList.emplace_back(config);
 }
-// updates the alert for the current. automatically updates to true if has been over 3s or false.
-void UpdateCurrentAlert(MotorAlertConfig& config, units::ampere_t motorCurrent) {
-  if (motorCurrent > config.maxCurrent) {
-    config.highCurrentTimer.Start();
-    if (config.highCurrentTimer.Get() > 3_s) {
-      config.currentAlert.Set(true);
-      config.recordedCurrentAlert.Set(true);
-    }
 
+// Check Every Alert for the Motor and Set them accordingly
+void MotorCheck(auto Motor, AlertConfig& config) {
+  // Check Temperature
+  if (Motor.GetTemperature() >= config.maxDegrees) {
+    config.responsiveHighTemperatureAlert.Set(true);
+    config.tempuratureReachedCount++;
+    config.reachedTemperatureAlert.Set(true);
   } else {
-    config.currentAlert.Set(false);
+    config.responsiveHighTemperatureAlert.Set(false);
+  }
+
+  // Check High Current
+  if (Motor.GetStatorCurrent() >= config.maxCurrent) {
+    config.responsiveHighCurrentAlert.Set(true);
+    config.highCurrentTimer.Start();
+    if (config.highCurrentTimer.HasElapsed(1_s)) {
+      config.highCurrentTimer.Stop();
+      config.highCurrentTimer.Reset();
+      config.highCurrentReachedCount++;
+      config.reachedHighCurrentAlert.Set(true);
+    }
+  } else {
+    config.responsiveHighCurrentAlert.Set(false);
+    config.highCurrentTimer.Stop();
     config.highCurrentTimer.Reset();
   }
+  // Check Low Current
+  if (Motor.GetStatorCurrent() <= config.minCurrent) {
+    config.responsiveLowCurrentAlert.Set(true);
+    if (config.highCurrentTimer.HasElapsed(1_s)) {
+      config.highCurrentTimer.Stop();
+      config.highCurrentTimer.Reset();
+      config.lowCurrentReachedCount++;
+      config.reachedLowCurrentAlert.Set(true);
+    }
+  } else {
+    config.responsiveLowCurrentAlert.Set(false);
+  }
+}
+// Command to Force Remove All Alerts, used for testing and to reset the alert counts
+frc2::CommandPtr ForceRemoveAllAlerts() {
+  return frc2::cmd::RunOnce([] {
+    for (std::weak_ptr<AlertConfig>& configWeak : configList) {
+      if (auto config = configWeak.lock()) {  // lock once
+      }
+    }
+  })
+    .WithName("Force Remove All Alerts")
+    .IgnoringDisable(true);
 }
 
 }  // namespace AlertController
--- src/main/include/utilities/AlertController.h
+++ src/main/include/utilities/AlertController.h
@@ -1,24 +1,57 @@
 #pragma once
 #include <frc/Alert.h>
 #include <frc/Timer.h>
+#include <frc2/command/Commands.h>
+#include <frc2/command/SubsystemBase.h>
 
+#include <memory>
 #include <units/current.h>
 #include <units/temperature.h>
 
 namespace AlertController {
-struct MotorAlertConfig {
-  frc::Alert temperatureAlert;
-  frc::Alert currentAlert;
-  frc::Alert recordedTemperatureAlert;
-  frc::Alert recordedCurrentAlert;
+
+struct AlertConfig {
+  std::string motorString;
   units::celsius_t maxDegrees;
+
   units::ampere_t maxCurrent;
+  units::ampere_t minCurrent;
+
+  // Tempurature Alerts//
+  frc::Alert responsiveHighTemperatureAlert = frc::Alert(motorString + ": " + "!HIGH TEMP!", frc::Alert::AlertType::kWarning);
+  frc::Alert reachedTemperatureAlert = frc::Alert(motorString + "Reached Max Temperature Threshold: " + std::to_string(tempuratureReachedCount) + (tempuratureReachedCount > 1 ? " Times" : " Time") , frc::Alert::AlertType::kWarning);
+
+  // Current Alerts//
+  frc::Alert responsiveHighCurrentAlert = frc::Alert(motorString + ": " + "!HIGH CURRENT!", frc::Alert::AlertType::kWarning);
+  frc::Alert reachedHighCurrentAlert = frc::Alert(motorString + "Reached Max Current Threshold: " + std::to_string(highCurrentReachedCount) + (highCurrentReachedCount > 1 ? " Times" : " Time") , frc::Alert::AlertType::kWarning);
+  
+  frc::Alert responsiveLowCurrentAlert = frc::Alert(motorString + ": " + "!LOW CURRENT!", frc::Alert::AlertType::kWarning);
+  frc::Alert reachedLowCurrentAlert = frc::Alert(motorString + "Reached Min Current Threshold: " + std::to_string(lowCurrentReachedCount) + (lowCurrentReachedCount > 1 ? " Times" : " Time") , frc::Alert::AlertType::kWarning);
+
+  // Timers for Current
   frc::Timer highCurrentTimer = frc::Timer();
+
+ // Counts for how many times the motor has reached the thresholds
+  int tempuratureReachedCount = 0;
+  int highCurrentReachedCount = 0;
+  int lowCurrentReachedCount = 0;
+
+  AlertConfig(){
+    responsiveHighTemperatureAlert.SetText(motorString + ": " + "!HIGH TEMP!");
+    reachedTemperatureAlert.SetText(motorString + "Reached Max Temperature Threshold: " + std::to_string(tempuratureReachedCount) + " Times" );
+
+   responsiveHighCurrentAlert.SetText(motorString + ": " + "!HIGH CURRENT!");
+   reachedHighCurrentAlert.SetText(motorString + "Reached Max Current Threshold: " + std::to_string(highCurrentReachedCount) + " Times" );
+   responsiveLowCurrentAlert.SetText(motorString + ": " + "!LOW CURRENT!");
+   reachedLowCurrentAlert.SetText(motorString + "Reached Min Current Threshold: " + std::to_string(lowCurrentReachedCount) + " Times" );  
+  }
 };
 
-// updates the alert for the temperature. automatically updates to true or false
-void UpdateTemperatureAlert(MotorAlertConfig& config, units::celsius_t motorTemperature);
 
-// updates the alert for the current. automatically updates to true if has been over 3s or false.
-void UpdateCurrentAlert(MotorAlertConfig& config, units::ampere_t motorCurrent);
+void RegisterAlertConfig(std::weak_ptr<AlertConfig> config);
+
+void MotorCheck(auto Motor, AlertConfig& config);
+
+frc2::CommandPtr ForceRemoveAllAlerts();
+
 };  // namespace AlertController
