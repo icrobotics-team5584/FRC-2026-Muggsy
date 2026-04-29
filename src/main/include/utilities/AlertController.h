e7c5ef2f3021a568de7f75f6d1e41f971c7feae1
Noah <noahrjenkinson@gmail.com>
2026-04-29 20:19:01 +1000

    Clang Format

--- src/main/include/utilities/AlertController.h
+++ src/main/include/utilities/AlertController.h
@@ -18,35 +18,49 @@ struct AlertConfig {
   units::ampere_t minCurrent;
 
   // Tempurature Alerts//
-  frc::Alert responsiveHighTemperatureAlert = frc::Alert(motorString + ": " + "!HIGH TEMP!", frc::Alert::AlertType::kWarning);
-  frc::Alert reachedTemperatureAlert = frc::Alert(motorString + "Reached Max Temperature Threshold: " + std::to_string(tempuratureReachedCount) + (tempuratureReachedCount > 1 ? " Times" : " Time") , frc::Alert::AlertType::kWarning);
+  frc::Alert responsiveHighTemperatureAlert =
+    frc::Alert(motorString + ": " + "!HIGH TEMP!", frc::Alert::AlertType::kWarning);
+  frc::Alert reachedTemperatureAlert = frc::Alert(
+    motorString + "Reached Max Temperature Threshold: " + std::to_string(tempuratureReachedCount) +
+      (tempuratureReachedCount > 1 ? " Times" : " Time"),
+    frc::Alert::AlertType::kWarning);
 
   // Current Alerts//
-  frc::Alert responsiveHighCurrentAlert = frc::Alert(motorString + ": " + "!HIGH CURRENT!", frc::Alert::AlertType::kWarning);
-  frc::Alert reachedHighCurrentAlert = frc::Alert(motorString + "Reached Max Current Threshold: " + std::to_string(highCurrentReachedCount) + (highCurrentReachedCount > 1 ? " Times" : " Time") , frc::Alert::AlertType::kWarning);
-  
-  frc::Alert responsiveLowCurrentAlert = frc::Alert(motorString + ": " + "!LOW CURRENT!", frc::Alert::AlertType::kWarning);
-  frc::Alert reachedLowCurrentAlert = frc::Alert(motorString + "Reached Min Current Threshold: " + std::to_string(lowCurrentReachedCount) + (lowCurrentReachedCount > 1 ? " Times" : " Time") , frc::Alert::AlertType::kWarning);
+  frc::Alert responsiveHighCurrentAlert =
+    frc::Alert(motorString + ": " + "!HIGH CURRENT!", frc::Alert::AlertType::kWarning);
+  frc::Alert reachedHighCurrentAlert = frc::Alert(
+    motorString + "Reached Max Current Threshold: " + std::to_string(highCurrentReachedCount) +
+      (highCurrentReachedCount > 1 ? " Times" : " Time"),
+    frc::Alert::AlertType::kWarning);
+
+  frc::Alert responsiveLowCurrentAlert =
+    frc::Alert(motorString + ": " + "!LOW CURRENT!", frc::Alert::AlertType::kWarning);
+  frc::Alert reachedLowCurrentAlert = frc::Alert(
+    motorString + "Reached Min Current Threshold: " + std::to_string(lowCurrentReachedCount) +
+      (lowCurrentReachedCount > 1 ? " Times" : " Time"),
+    frc::Alert::AlertType::kWarning);
 
   // Timers for Current
   frc::Timer highCurrentTimer = frc::Timer();
 
- // Counts for how many times the motor has reached the thresholds
+  // Counts for how many times the motor has reached the thresholds
   int tempuratureReachedCount = 0;
   int highCurrentReachedCount = 0;
   int lowCurrentReachedCount = 0;
 
-  AlertConfig(){
+  AlertConfig() {
     responsiveHighTemperatureAlert.SetText(motorString + ": " + "!HIGH TEMP!");
-    reachedTemperatureAlert.SetText(motorString + "Reached Max Temperature Threshold: " + std::to_string(tempuratureReachedCount) + " Times" );
+    reachedTemperatureAlert.SetText(motorString + "Reached Max Temperature Threshold: " +
+                                    std::to_string(tempuratureReachedCount) + " Times");
 
-   responsiveHighCurrentAlert.SetText(motorString + ": " + "!HIGH CURRENT!");
-   reachedHighCurrentAlert.SetText(motorString + "Reached Max Current Threshold: " + std::to_string(highCurrentReachedCount) + " Times" );
-   responsiveLowCurrentAlert.SetText(motorString + ": " + "!LOW CURRENT!");
-   reachedLowCurrentAlert.SetText(motorString + "Reached Min Current Threshold: " + std::to_string(lowCurrentReachedCount) + " Times" );  
+    responsiveHighCurrentAlert.SetText(motorString + ": " + "!HIGH CURRENT!");
+    reachedHighCurrentAlert.SetText(motorString + "Reached Max Current Threshold: " +
+                                    std::to_string(highCurrentReachedCount) + " Times");
+    responsiveLowCurrentAlert.SetText(motorString + ": " + "!LOW CURRENT!");
+    reachedLowCurrentAlert.SetText(motorString + "Reached Min Current Threshold: " +
+                                   std::to_string(lowCurrentReachedCount) + " Times");
   }
 };
-
 
 void RegisterAlertConfig(std::weak_ptr<AlertConfig> config);
 
