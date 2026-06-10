3e9aba86d4f42b000ef38ddfc2132ee02edf54be
furmea <diaschithira21@gmail.com>
2026-06-10 18:10:20 +1000

    added bounds checking

--- src/main/cpp/subsystems/SubDeploy.cpp
+++ src/main/cpp/subsystems/SubDeploy.cpp
@@ -82,7 +82,7 @@ frc2::CommandPtr SubDeploy::ExtendToLerp(double t) {
 frc2::CommandPtr SubDeploy::ManualExtendDown() {
   return frc2::cmd::StartEnd(
     [this] {
-      if (_hasZeroed) {
+      if (_hasZeroed && GetLength() > 0_m) {
         _motor.SetVoltage(-1_V);
       }
     },
@@ -96,7 +96,7 @@ frc2::CommandPtr SubDeploy::ManualExtendDown() {
 frc2::CommandPtr SubDeploy::ManualExtendUp() {
   return frc2::cmd::StartEnd(
     [this] {
-      if (_hasZeroed) {
+      if (_hasZeroed && GetLength() < MAX_LENGTH) {
         _motor.SetVoltage(1_V);
       }
     },
