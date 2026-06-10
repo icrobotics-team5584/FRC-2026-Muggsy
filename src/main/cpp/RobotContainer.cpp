0d7184fc0d3c1ba4224bbdef492980cee17df6a6
furmea <diaschithira21@gmail.com>
2026-06-10 20:38:33 +1000

    Reapply "SubVision"
    This reverts commit 460e4f7031406f6edae9b8c654b3e5d8295a19c6.

--- src/main/cpp/RobotContainer.cpp
+++ src/main/cpp/RobotContainer.cpp
@@ -9,6 +9,9 @@
 #include "subsystems/SubHood.h"
 #include "subsystems/SubIndexer.h"
 #include "subsystems/SubIntake.h"
+#include "subsystems/SubVision.h"
+
+#include "commands/VisionCommands.h"
 
 #include "utilities/Logger.h"
 
@@ -16,6 +19,7 @@
 
 RobotContainer::RobotContainer() {
   ConfigureBindings();
+  SubVision::GetInstance().SetDefaultCommand(cmd::AddVisionMeasurement());
   SubDrivebase::GetInstance().SetDefaultCommand(
     SubDrivebase::GetInstance().JoystickDrive(_driverController));
 }
--- src/main/include/utilities/ICCamera.h
+++ src/main/include/utilities/ICCamera.h
@@ -1,6 +1,7 @@
 // Copyright (c) FIRST and other WPILib contributors.
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
+#pragma once
 
 #include <frc/geometry/Translation3d.h>
 #include <frc2/command/Commands.h>
@@ -9,11 +10,10 @@
 #include <photon/PhotonPoseEstimator.h>
 #include <photon/simulation/PhotonCameraSim.h>
 
-#pragma once
-
 class ICCamera {
  public:
-  ICCamera(const std::string& name, frc::Transform3d botToCam, frc::AprilTagFieldLayout tagMap);
+  ICCamera(
+    const std::string_view& name, frc::Transform3d botToCam, frc::AprilTagFieldLayout tagMap);
 
   void Update();
 
