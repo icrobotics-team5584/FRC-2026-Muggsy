b3499f6f5b030da61dbae07041e1744eba82f85a
s-haveesh <shaveesh@gmail.com>
2026-06-20 20:30:28 +1000

    Format and implement ToggleBrakeCoast and EjectFuel

--- src/main/cpp/commands/FuelCommands.cpp
+++ src/main/cpp/commands/FuelCommands.cpp
@@ -1,31 +1,50 @@
 #include "commands/FuelCommands.h"
 
 #include "subsystems/SubDeploy.h"
-#include "subsystems/SubIntake.h"
-#include "subsystems/SubHood.h"
-#include "subsystems/SubShooter.h"
 #include "subsystems/SubDrivebase.h"
+#include "subsystems/SubFeeder.h"
+#include "subsystems/SubHood.h"
+#include "subsystems/SubIndexer.h"
+#include "subsystems/SubIntake.h"
+#include "subsystems/SubShooter.h"
 
-#include "utilities/ShotPlanner.h"
 #include "utilities/PoseHandler.h"
-
+#include "utilities/ShotPlanner.h"
 
 namespace cmd {
-    frc2::CommandPtr IntakeSequence() {
-        return SubDeploy::GetInstance().ExtendToLerp(1.0)
-            .AndThen(SubIntake::GetInstance().RunIntake());
-    }
-
-    frc2::CommandPtr ReverseIntakeSequence() {
-        return SubDeploy::GetInstance().ExtendToLerp(1.0)
-            .AndThen(SubIntake::GetInstance().RunReverseIntake());
-    }
-
-    frc2::CommandPtr StationaryShootAt(frc::Translation2d target) {
-    }
-
-    bool IsReadyToShoot() {
-      return SubHood::GetInstance().IsAtTarget() && SubShooter::GetInstance().IsReadyToShoot() &&
-             ShotPlanner::CalculateShotTarget(PoseHandler::GetInstance().GetPose()).shouldShoot;
-    }
+frc2::CommandPtr IntakeSequence() {
+  return SubDeploy::GetInstance().ExtendToLerp(1.0).AndThen(SubIntake::GetInstance().RunIntake());
 }
+
+frc2::CommandPtr ReverseIntakeSequence() {
+  return SubDeploy::GetInstance().ExtendToLerp(1.0).AndThen(
+    SubIntake::GetInstance().RunReverseIntake());
+}
+
+frc2::CommandPtr StationaryShootAt(frc::Translation2d target) {}
+
+bool IsReadyToShoot() {
+  return SubHood::GetInstance().IsAtTarget() && SubShooter::GetInstance().IsReadyToShoot() &&
+         ShotPlanner::CalculateShotTarget(PoseHandler::GetInstance().GetPose()).shouldShoot;
+}
+
+frc2::CommandPtr ToggleBrakeCoast() {
+  return frc2::cmd::StartEnd(
+    [] {
+      SubDrivebase::GetInstance().SetBrakeMode(false);
+      SubHood::GetInstance().SetBrakeMode(false);
+    },
+    [] {
+      SubDrivebase::GetInstance().SetBrakeMode(true);
+      SubHood::GetInstance().SetBrakeMode(true);
+    })
+    .IgnoringDisable(true)
+    .Until([] { return frc::DriverStation::IsEnabled(); });
+}
+
+frc2::CommandPtr EjectFuel() {
+  return frc2::cmd::Parallel(SubShooter::GetInstance().SpinSlowly(),
+    SubIntake::GetInstance().RunReverseIntake(), SubIndexer::GetInstance().SpinIndexer(),
+    SubFeeder::GetInstance().Feed(), SubHood::GetInstance().HoodToEjectAngle());
+}
+}  // namespace cmd
