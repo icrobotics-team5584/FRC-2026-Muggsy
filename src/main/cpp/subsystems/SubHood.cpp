40ee5a50b45e7664b98436f86c3abf266d9b1687
Will <wmccann2017@gmail.com>
2026-06-21 16:27:31 +1000

    Basic hood tune

--- src/main/cpp/subsystems/SubHood.cpp
+++ src/main/cpp/subsystems/SubHood.cpp
@@ -13,10 +13,10 @@ SubHood::SubHood() {
   rev::spark::SparkBaseConfig config;
   config.encoder.PositionConversionFactor(1 / GEAR_RATIO);
   config.encoder.VelocityConversionFactor(1 / GEAR_RATIO);
-  config.closedLoop.Pid(2, 0, 0);
-  config.closedLoop.feedForward.kS(0.6);
-  config.SmartCurrentLimit(10);
-  config.Inverted(false);
+  config.closedLoop.Pid(60, 0, 0);
+  config.closedLoop.feedForward.kS(0);
+  config.SmartCurrentLimit(35);
+  config.Inverted(true);
   config.SetIdleMode(rev::spark::SparkBaseConfig::IdleMode::kCoast);
   _hoodMotor.OverwriteConfig(config);
 
