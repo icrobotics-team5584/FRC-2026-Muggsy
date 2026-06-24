e563200a04c1fe82cd88a562090f5f5cb58fee4e
s-haveesh <shaveesh@gmail.com>
2026-06-24 19:57:11 +1000

    Add ShootWhenReady

--- src/main/cpp/commands/FuelCommands.cpp
+++ src/main/cpp/commands/FuelCommands.cpp
@@ -30,6 +30,17 @@ bool IsReadyToShoot() {
          ShotPlanner::CalculateShotTarget(PoseHandler::GetInstance().GetPose()).shouldShoot;
 }
 
+frc2::CommandPtr ShootWhenReady() {
+  return frc2::cmd::WaitUntil([] { return IsReadyToShoot(); })
+    .AndThen(frc2::cmd::Parallel(
+      SubFeeder::GetInstance().Feed(),
+      SubIndexer::GetInstance().SpinIndexer(),
+      SubIntake::GetInstance().RunIntake()
+    ).Until([] {
+      return !IsReadyToShoot();
+    })).Repeatedly();
+};
+
 frc2::CommandPtr ToggleBrakeCoast() {
   return frc2::cmd::StartEnd(
     [] {
